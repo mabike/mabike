712727b32de353f52e7348ac8e931f72ad492528
Tom Brückner <tom.brueckner@gmx.net>
2016-04-17 08:18:45 +0200

    optimized bluetooth simulation

--- webapp/packages/philips-hue-interface/methods/flash-lights.js
+++ webapp/packages/philips-hue-interface/methods/flash-lights.js
@@ -7,10 +7,6 @@ const url = 'http://10.10.1.90/api/7iLce3inNTCkX5cbnRMRQJNeVoeLKPIDqGg9Avas/ligh
 //   '{"on":true, "sat":254, "bri":254,"hue":56100}'
 // ];
 const on = [
-  '{"on":true, "sat":254, "bri":254,"hue":46920}',
-  '{"on":true, "sat":254, "bri":254,"hue":46920}',
-  '{"on":true, "sat":254, "bri":254,"hue":46920}',
-  '{"on":true, "sat":254, "bri":254,"hue":46920}',
   '{"on":true, "sat":254, "bri":254,"hue":46920}'
 ];
 const off = '{"on":false, "sat":254, "bri":254,"hue":46920}';
@@ -27,7 +23,7 @@ Meteor.methods({
       Meteor.sleep(500);
       for (var k = 1; k <= 3; k++) {
         HTTP.call('PUT', url + k + '/state', {
-          content: on[i % 5]
+          content: on[i % on.length]
         });
       }
       Meteor.sleep(500);
