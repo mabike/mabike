aa8c08d67819584547200ada50e9297fa1cc1fa0
Tom Brückner <tom.brueckner@gmx.net>
2016-04-17 07:00:31 +0200

    better name randomization

--- webapp/packages/fake-notifications/templates/dialog-bike-found.js
+++ webapp/packages/fake-notifications/templates/dialog-bike-found.js
@@ -1,9 +1,13 @@
-const firstName = Random.choice([
-  'David', 'Elias', 'Marie', 'Sophia', 'Anna', 'Ben', 'Emil', 'Laura', 'Leonie', 'Hanna'
-]);
+/* global ModalDialog */
+
+var firstName;
 
 Template.dialogBikeFound.onCreated(function() {
   const instance = this;
+  ModalDialog.onHide = resetDialog;
+  firstName = Random.choice([
+    'David', 'Elias', 'Marie', 'Sophia', 'Anna', 'Ben', 'Emil', 'Laura', 'Leonie', 'Hanna'
+  ]);
   instance.nameRank = new ReactiveVar();
   Meteor.call('maOpenData:getNameRank', firstName, function(error, result) {
     instance.nameRank.set(result);
@@ -16,5 +20,16 @@ Template.dialogBikeFound.helpers({
   },
   firstName() {
     return firstName;
+  }
+});
+
+var resetDialog = function() {
+  // This allows the requester to send another request from the user profile page
+  ModalDialog.template.set();
+};
+
+Template.dialogBikeFound.onDestroyed(function() {
+  if (ModalDialog.onHide === resetDialog) {
+    ModalDialog.onHide = undefined;
   }
 });
